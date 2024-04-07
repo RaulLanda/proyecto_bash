72ef6b55ae85b1d03bf6cdd4f3c10c06be6efb40
César Raúl Landa Aponte <cr.landaa@gmail.com>
2024-04-07 10:11:02 -0700

    Parametrizar el pais

--- pregunton.sh
+++ pregunton.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+elpais="USA"
 echo "Hola, yo pregunto cosas"
 
-echo "Cual es la poblacion de Mexico?"
-grep -i "mexico" paises.txt | cut -d"," -f2
+echo "Cual es la poblacion de $elpais?"
+grep  "$elpais" paises.txt | cut -d"," -f2
 
-echo "Cual es la clima de Mexico?"
-grep -i "mexico" paises.txt | cut -d"," -f3
+echo "Cual es la clima de $elpais?"
+grep  "$elpais" paises.txt | cut -d"," -f3
